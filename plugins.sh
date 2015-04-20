f4455d9b1d99e696f8ef9d39fc1a365e59da7b80
zhenkunou <ryan.ouzhenkun@gmail.com>
2015-04-20 14:41:08 +0800

    Refactor project config

--- plugins.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-cordova plugin add com.ionic.keyboard
-cordova plugin add org.apache.cordova.device
-cordova plugin add org.apache.cordova.console
-cordova plugin add org.apache.cordova.inappbrowser
