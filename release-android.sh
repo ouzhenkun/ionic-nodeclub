26243a9d105540db09a5ff41eedc98cd089b3963
zhenkunou <ryan.ouzhenkun@gmail.com>
2015-05-24 12:59:11 +0800

    Adjust menu style and update version

--- release-android.sh
+++ release-android.sh
@@ -3,8 +3,9 @@
 #ionic build --release android
 
 VERSION=$1
-NAME=CordovaApp
 APK_PATH=platforms/android/ant-build
-rm $APK_PATH/$NAME-$VERSION.apk
-Jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android-release-key.keystore $APK_PATH/$NAME-release-unsigned.apk alias_name
-~/android/sdk/build-tools/21.1.2/zipalign -v 4 $APK_PATH/$NAME-release-unsigned.apk $APK_PATH/ionic-nodeclub-$VERSION.apk
+NAME=$APK_PATH/CordovaApp-release-unsigned.apk
+OUTPUT=$APK_PATH/ionic-nodeclub-$VERSION.apk
+rm $OUTPUT
+Jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android-release-key.keystore $NAME alias_name
+~/android/sdk/build-tools/21.1.2/zipalign -v 4 $NAME $OUTPUT
